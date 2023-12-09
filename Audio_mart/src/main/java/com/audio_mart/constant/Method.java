b41459d4f6222ef0ee7df0112c699696fb2ab83e
귀띵 <ok88eun77@naver.com>
2023-12-10 02:21:35 +0900

    회원가입 message처리

---
+++ Audio_mart/src/main/java/com/audio_mart/constant/Method.java
@@ -0,0 +1,5 @@
+package com.audio_mart.constant;
+
+public enum Method {
+	GET, POST, PUT, PATCH, DELETE
+}
--- Audio_mart/src/main/java/com/audio_mart/controller/MemberController.java
+++ Audio_mart/src/main/java/com/audio_mart/controller/MemberController.java
@@ -7,11 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.audio_mart.constant.Method;
 import com.audio_mart.domain.MemberDTO;
 import com.audio_mart.service.MemberService;
+import com.audio_mart.util.UiUtils;
 
 @Controller
-public class MemberController {
+public class MemberController extends UiUtils{
 	
 	@Autowired
 	private MemberService memberService;
@@ -19,27 +21,25 @@ public class MemberController {
 	@GetMapping(value = "/member/login.do")
 	public String openSignup(Model model) {
 		model.addAttribute("newMember", new MemberDTO());
-		System.out.println("get 됨");
 		return "member/signup";
 	}
 	
 	@PostMapping(value = "/member/register.do")
-	public String registerMember(final MemberDTO params) {
+	public String registerMember(final MemberDTO params, Model model) {
 		System.out.println("post됨");
 		try {
 			boolean isRegistered = memberService.signup(params);
 			if (isRegistered == false) {
-				System.out.println("등록 실패");
-				return "member/signup";
+				return showMessageWithRedirect("회원가입에 실패하였습니다.", "/member/login.do", Method.GET, null, model);
 			}
 		} catch (DataAccessException e) {
-			System.out.println("데베 문제");
-			return "member/signup";
+			return showMessageWithRedirect("데이터베이스 문제 발생", "/member/login.do", Method.GET, null, model);
 		} catch (Exception e) {
-			System.out.println("시스템 문제");
+			return showMessageWithRedirect("시스템 문제 발생", "/member/login.do", Method.GET, null, model);
 		}
-		System.out.println("회원가입 완료");
-		return "member/signup";
+
+		model.addAttribute("newMember", new MemberDTO());
+		return showMessageWithRedirect("회원가입이 완료되었습니다.", "/member/login.do", Method.GET, null, model);
 	}
 	
 
