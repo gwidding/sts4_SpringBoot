aeac611560d6ecdfe403db8cde27047f8b649688
gwidding <135992700+gwidding@users.noreply.github.com>
2023-12-20 20:57:52 +0900

    상품 등록 서비스Impl 구현 필요

---
+++ Audio_mart/src/main/java/com/audio_mart/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.audio_mart.service;
+
+import com.audio_mart.domain.ProductDTO;
+
+public interface ProductService {
+	
+	// 상품 등록하기
+	public boolean uploadProduct(ProductDTO params);
+	// 상품 수정하기
+	// 상품 삭제하기
+	// 상품 목록 가져오기
+	
+
+}
---
+++ Audio_mart/src/main/java/com/audio_mart/service/ProductServiceImpl.java
@@ -0,0 +1,13 @@
+package com.audio_mart.service;
+
+import com.audio_mart.domain.ProductDTO;
+
+public class ProductServiceImpl implements ProductService{
+
+	@Override
+	public boolean uploadProduct(ProductDTO params) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
