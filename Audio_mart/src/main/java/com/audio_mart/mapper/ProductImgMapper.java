36d0487c7ea214148552ee22050355efda3e6ce3
gwidding <135992700+gwidding@users.noreply.github.com>
2024-01-02 10:48:38 +0900

    매퍼 list, detailImg 메서드 추가

--- Audio_mart/src/main/java/com/audio_mart/mapper/ProductImgMapper.java
+++ Audio_mart/src/main/java/com/audio_mart/mapper/ProductImgMapper.java
@@ -1,15 +1,26 @@
 package com.audio_mart.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
 import com.audio_mart.domain.ProductImgDTO;
 
+@Mapper
 public interface ProductImgMapper {
 	
 	// 이미지 등록( file, path )
 	public boolean insertImg(ProductImgDTO params);
+	// 한 이미지 정보 가져오기
+	public ProductImgDTO imgDetail(Long imgId);
 	// 이미지 수정
 	public boolean updateImg(ProductImgDTO params);
 	// 이미지 삭제
-	public boolean deleteImg(Long imageId);
+	public boolean deleteImg(Long imgId);
+	// 이미지 목록
+	public List<ProductImgDTO> getImgList(Long productId);
+	// 이미지 개수 카운팅
+	public int selectImgTotalCnt(Long productId);
 	
 
 }
